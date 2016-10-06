3ab2c5a9a301e06c31fdf4492c39f6e82aebe04b
fly <807065932@qq.com>
2016-10-06 22:11:58 +0800

    Initial commit

---
+++ source/production/java/com/wrox/site/HelloController.java
@@ -0,0 +1,22 @@
+package com.wrox.site;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+
+@Controller
+public class HelloController {
+
+    @Inject
+    private GreetingService greetingService;
+
+    @ResponseBody
+    @RequestMapping(value = "/test", params = {"name"})
+    public String helloName(@RequestParam("name") String name) {
+        return this.greetingService.getGreeting(name);
+    }
+
+}
